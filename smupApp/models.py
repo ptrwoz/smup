ada99c0f38a4480693f31bfc0e3876d7ab5e14b5
ptrwoz <ptrwoz@gmail.com>
2022-01-21 14:52:14 +0100

    heroky cdn (final? not yet 3)

--- smupApp/models.py
+++ smupApp/models.py
@@ -50,13 +50,13 @@ class AuthPermission(models.Model):
 class AuthUser(models.Model):
     password = models.CharField(max_length=128)
     last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
+    is_superuser = models.IntegerField()
     username = models.CharField(unique=True, max_length=150)
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
     date_joined = models.DateTimeField()
 
     class Meta:
