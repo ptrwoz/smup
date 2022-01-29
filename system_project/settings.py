8776db9077ddf95e4c7515cb58759d646ba505a3
ptrwoz <ptrwoz@gmail.com>
2022-01-30 00:27:48 +0100

    [30.01.2022]

--- system_project/settings.py
+++ system_project/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'system_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-'''DATABASES = {
+DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'system_project.wsgi.application'
         'PORT': '5432',
     }
 }
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'system1',
@@ -97,7 +97,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
-}'''
+}
 
 DATABASES = {
     'default': {
@@ -108,7 +108,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
-}
+}'''
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
